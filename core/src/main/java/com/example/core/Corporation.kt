ef1e95f3570325c45f829dd84b691f52606c85d0
DADJOD <dassdedy@gmail.com>
2026-08-03 13:07:52 +0300

    Initial commit

--- core/src/main/java/com/example/core/Corporation.kt
+++ core/src/main/java/com/example/core/Corporation.kt
@@ -7,6 +7,4 @@ class Corporation {
 fun main() {
     var assistant = Assistant()
     assistant.bringCoffee(3, "Latte")
-    assistant.bringCoffee(2, "Cappuccino")
-    assistant.bringCoffee(1, "Espresso")
 }
